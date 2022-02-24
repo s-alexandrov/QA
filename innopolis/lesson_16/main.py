d8d3eff782d2ea6148f85347971f7c6369a649f8
Sergey Alexandrov <s.alexandrov@ngs.ru>
2022-02-24 18:21:17 +0700

    доработка игры герой и чудовища

--- innopolis/lesson_16/main.py
+++ innopolis/lesson_16/main.py
@@ -7,38 +7,25 @@ monster_counter = 0
 
 def player_turn() -> int:
     """Генерируем случайное число от 1 до 3: бой с монстром, получает яблочко или меч."""
-    move = int(random.randint(1, 3))
+    move = random.randint(1, 3)
     return move
 
 
 def apple() -> int:
     """Определяем сколько бонусов получит рыцарь к здоровью."""
-    add_hp = int(random.randint(1, 5))
+    add_hp = random.randint(1, 5)
     global hp
     hp += add_hp
-    print(
-        "Вы получили яблочко +",
-        add_hp,
-        "hp. Кол-во ваших жизней увеличилось на",
-        add_hp,
-        "и теперь =",
-        hp,
-        "\n",
-    )
+    print("Вы получили яблочко +", add_hp, "hp. Кол-во ваших жизней увеличилось на", add_hp, "и теперь =", hp, "\n",)
     return add_hp
 
 
 def sword() -> int:
     """Выпадение меча. Игрок может взять меч(1) или отказаться(2)"""
     global attack
-    add_attack = int(random.randint(5, 20))
-    print(
-        "Вы нашли меч с силой атаки:",
-        add_attack,
-        "Сейчас ваша сила атаки =",
-        attack,
-        "\nНажмите 1 (взять меч) или 2 (выбросить меч)",
-    )
+    add_attack = random.randint(5, 20)
+    print("Вы нашли МЕЧ с силой атаки:", add_attack, "Сейчас ваша сила атаки =", attack,
+          "\nНажмите 1 (взять меч) или 2 (выбросить меч)",)
     take_sword_or_not = int(input())
     if take_sword_or_not == 1:
         attack = add_attack
@@ -48,8 +35,9 @@ def sword() -> int:
         print("Вы выбросили меч, ваша сила атаки осталась =", attack, "\n")
         return attack
     else:
-        print("Вы ввели неверное значение, введите 1 или 2")
-        take_sword_or_not = int(input())
+        while take_sword_or_not != 1 and take_sword_or_not != 2:  # Добиваемся ввода правильного числа пользователя
+            print("Вы ввели неверное значение, введите 1 или 2")
+            take_sword_or_not = int(input())
         return take_sword_or_not
 
 
@@ -59,49 +47,30 @@ def game():
     global hp  # текущее состояние здоровье героя
     global monster_counter  # счетчик поверженных героем чудовищ
     while monster_counter < 10:
-        g_player_turn = (
-            player_turn()
-        )  # Вызываем функцию которая определяет 1-монстр, 2-яблоко, 3-меч
+        g_player_turn = player_turn()  # Вызываем функцию которая определяет 1-монстр, 2-яблоко, 3-меч
         if g_player_turn == 1:
-            monster_health = int(random.randint(1, 20))  # жизни монстра
-            monster_power = int(random.randint(1, 15))  # сила удара монстра
-            print(
-                "БОЙ! Вы встретили монстра с",
-                monster_health,
-                "жизнями и силой удара:",
-                monster_power,
-                "\nСейчас кол-во ваших жизней:",
-                hp,
-                "и сила атаки:",
-                attack,
-                "\nВведите 1, чтобы атаковать или 2, чтобы убежать",
-            )
+            monster_health = random.randint(1, 20)  # жизни монстра
+            monster_power = random.randint(1, 15)  # сила удара монстра
+            print("БОЙ! Вы встретили монстра с", monster_health, "жизнями и силой удара:", monster_power,
+                  "\nСейчас кол-во ваших жизней:", hp, "и сила атаки:", attack,
+                  "\nВведите 1, чтобы атаковать или 2, чтобы убежать")
             fight_or_run = int(input())
+            while fight_or_run != 1 and fight_or_run != 2:  # Добиваемся ввода правильного числа пользователя
+                print("Вы ввели неверное значение, введите 1 или 2")
+                fight_or_run = int(input())
             if fight_or_run == 1:
-                if attack > monster_health:
-                    hp -= monster_power  # Уменьшаем кол-во жизни рыцаря отнимая силы атаки монстра
-                    if hp <= 0:  # Если после боя у рыцаря <= 0 hp - он тоже умирает
-                        print(
-                            "ПОРАЖЕНИЕ! После атаки кол-во ваших жизней стало =",
-                            hp,
-                            "Попробуйте еще раз",
-                        )
+                while hp > 0 and monster_health > 0:
+                    hp -= monster_power  # Уменьшаем кол-во жизни рыцаря отнимая силу атаки монстра
+                    monster_health -= attack  # Уменьшаем кол-во жизни монстра отнимая силу атаки рыцаря
+                    if hp > monster_health and hp > 0:
+                        player_turn()
+                    if hp <= 0:
+                        print("ПОРАЖЕНИЕ! Попробуйте сыграть еще раз.")
                         return False
-                    else:
-                        monster_counter += 1  # Увеличиваем счетчик побед
-                        print(
-                            "ПОБЕДА! Вы победили уже",
-                            monster_counter,
-                            "раз. Теперь ваше здоровье =",
-                            hp,
-                            "\n",
-                        )
-                else:
-                    print("ПОРАЖЕНИЕ! Попробуйте сыграть еще раз")
-                    return False
-            if (
-                fight_or_run == 2
-            ):  # Если игрок не берет меч - опять выкидываем числло от 1 до 3
+                monster_counter += 1
+                print("ПОБЕДА! Кол-во побед:", monster_counter, "из 10. Ваше здоровье:", hp,
+                      "Здоровье монстра:", monster_health, "\n")
+            if fight_or_run == 2:
                 player_turn()
         if g_player_turn == 2:  # Вызываем функцию получения яблочка
             apple()
